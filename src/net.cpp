efdfbae9fc722a2e9d01e89364ff2cd718610d63
Kylian Bos <Kylian.bos@gmail.com>
2023-12-23 18:03:03 +0100

    Fix missing ;

--- src/net.cpp
+++ src/net.cpp
@@ -48,7 +48,7 @@ uint64 nLocalServices = (fClient ? 0 : NODE_NETWORK);
 CAddress addrLocalHost("0.0.0.0", 0, false, nLocalServices);
 CNode* pnodeLocalHost = NULL;
 uint64 nLocalHostNonce = 0;
-boost::array<int, 10> vnThreadsRunning
+boost::array<int, 10> vnThreadsRunning;
 SOCKET hListenSocket = INVALID_SOCKET;
 
 vector<CNode*> vNodes;
